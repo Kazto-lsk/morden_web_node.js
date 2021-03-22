c0bc5057129268b8f0a71a9a40d418d6a59b4005
LuisK <47233861+Kazto-lsk@users.noreply.github.com>
2021-03-22 10:16:04 +0900

    1교시 전체 업로드

---
+++ 2021-03-22/1교시/process_object.js
@@ -0,0 +1,14 @@
+/*
+
+    Process 객체
+
+    - argv = 실행 매개변수를 나타냄
+    - env = 실행되는 컴퓨터의 환경과 관련된 정보 출력
+    - version = node.js의 버전
+    - versions = node.js와 종속된 프로그램 버전을 출력
+    - arch = 프로세서의 아키텍쳐를 출력
+    - platform = 플랫폼을 나타냅니다.
+
+*/
+
+
